1ef314d47ccceea08ee045acda2120528a475420
hector guevara <hectorguevara@hectors-MBP.fios-router.home>
2019-01-14 13:03:17 -0500

    changed fm fetch

--- src/Fm.js
+++ src/Fm.js
@@ -1,32 +1,65 @@
 import React from "react";
 
 class FootballM extends React.Component {
-  state = {
-    fm: []
-  };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      error: null,
+      isLoaded: false,
+      fm: []
+    };
+  }
   async componentDidMount() {
     try {
       const res = await fetch("https://top-back.herokuapp.com/api/fm/");
       const fm = await res.json();
       this.setState({
+        isLoaded: true,
         fm
       });
     } catch (e) {
       console.log(e);
     }
   }
+
+  // componentDidMount() {
+  //   fetch("https://top-back.herokuapp.com/api/fm/")
+  //     .then(res => res.json())
+  //     .then(
+  //       results => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           fm: results.fm
+  //         });
+  //       },
+  //       error => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           error
+  //         });
+  //       }
+  //     );
+  // }
+
   render() {
-    return (
-      <div>
-        {this.state.fm.map(item => (
-          <div key={item.id}>
-            <h1>{item.challenge}</h1>
-            <span>{item.writeup}</span>
-          </div>
-        ))}
-      </div>
-    );
+    const { error, isLoaded, fm } = this.state;
+    if (error) {
+      return <div>Error</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {fm.map(item => (
+            <div key={item.id}>
+              <h1>{item.challenge}</h1>
+              <span>{item.writeup}</span>
+            </div>
+          ))}
+        </div>
+      );
+    }
   }
 }
 
